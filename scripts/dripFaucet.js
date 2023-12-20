6490e23559276b67806c9deb2f9cb9184c1871f5
styliann-eth <ns2808@protonmail.com>
2023-12-20 18:27:29 +0000

    Streamlined token addresses

--- scripts/dripFaucet.js
+++ scripts/dripFaucet.js
@@ -1,9 +1,10 @@
+const contractAddresses = require('../constants/contractAddresses.json');
 const hre = require('hardhat');
 
 async function main() {
   // Get the faucet contract instance deployed on etherlinkTestnet
-  const faucetAddress = '0x35287bE77EAa21874dF2f5738fBd0E8C1E61df45';
-  const tokenAddress = '0x1A71f491fb0Ef77F13F8f6d2a927dd4C969ECe4f'; // eUSD token address on etherlinkTestnet
+  const faucetAddress = contractAddresses.Faucet;
+  const tokenAddress = contractAddresses.eUSD;
 
   const Faucet = await ethers.getContractFactory('Faucet');
   const faucet = await Faucet.attach(faucetAddress);
@@ -11,7 +12,6 @@ async function main() {
   // Call the requestTokens() function of the deployed Faucet contract
   await faucet.requestTokens(tokenAddress);
 
-  // const tokenAddress = await faucet.token();
   const Token = await ethers.getContractFactory('Token');
   const token = await Token.attach(tokenAddress);
   const tokenSymbol = await token.symbol();
